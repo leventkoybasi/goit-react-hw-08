e73fbb256a598ef1eaabbdf3f1f0be5984c07ecd
Levent KÖYBAŞI <leventkoybasi@hotmail.com>
2025-02-25 16:17:24 -0800

    feat: implement lazy loading for pages and wrap App component with BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 import Background from './components/Background';
 
+//lazy load pages
+const HomePage = lazy(() => import('./pages/Home'));
+const LoginPage = lazy(() => import('./pages/Login'));
+const ContactPage = lazy(() => import('./pages/Contact'));
+
+//components
 import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import Login from './pages/Login';
 import Github from './components/Github';
 
 function App() {
   return (
-    <Router>
-      <Background />
-      <div style={{ position: 'relative', zIndex: 1 }} className='container m-auto'>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/login' element={<Login />} />
-        </Routes>
-        <Github />
-      </div>
-    </Router>
+    <>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Background />
+        <div style={{ position: 'relative', zIndex: 1 }} className='container m-auto'>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/contact' element={<ContactPage />} />
+            <Route path='/login' element={<LoginPage />} />
+          </Routes>
+          <Github />
+        </div>
+      </Suspense>
+    </>
   );
 }
 
